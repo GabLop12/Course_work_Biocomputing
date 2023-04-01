8a8d39bb4e487009f62febc8b6b47129d69f490a
GabLop12 <126816591+GabLop12@users.noreply.github.com>
2023-04-01 04:39:11 +0100

    Update Test_plot.py
    comment

--- plots/Test_plot.py
+++ plots/Test_plot.py
@@ -1,14 +1,15 @@
 import unittest
 
 import matplotlib
-
+# import the function to be tested from the project
 from github.my_project1.plots.plots import plot
 
-
+# define an empty function
 def isintance(fig, Figure):
     pass
 
-
+# define a unit test class that inherits from `unittest.TestCase`
 class TestPlot(unittest.TestCase):
+    # define a test method that checks if the function runs without error
     def test_plot_runs(self):
         assert plot() is None
